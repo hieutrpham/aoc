da7a553020b6846d5a8285bf07e53fc4ef4e78af
Hieu Pham <hieutrungpham91@gmail.com>
2026-01-06 16:38:35 +0200

    feat: day2 part2

--- 2025/day2/day2.go
+++ 2025/day2/day2.go
@@ -14,7 +14,7 @@ func check_err(e error) {
 	}
 }
 
-func get_valid_id(s string) int {
+func part1(s string) int {
 	count := 0
 	rangeid := strings.Split(s, "-")
 	startid := rangeid[0]
@@ -39,7 +39,32 @@ func get_valid_id(s string) int {
 	return count
 }
 
-var count = 0
+func part2(s string) int {
+	count := 0
+	rangeid := strings.Split(s, "-")
+	startid := rangeid[0]
+	endid := rangeid[1]
+	startnum, err := strconv.Atoi(startid)
+	check_err(err)
+	endnum, err := strconv.Atoi(endid)
+	check_err(err)
+
+	for num := startnum; num <= endnum; num++ {
+		s := strconv.Itoa(num)
+		s_len := len(s)
+		for i := 1; i <= s_len/2; i++ {
+			substr_count := strings.Count(s, s[:i])
+			if substr_count >= 2 && i*substr_count == s_len {
+				count += num
+				break
+			}
+		}
+	}
+	return count
+}
+
+var count_part_1 = 0
+var count_part_2 = 0
 
 func main() {
 	args := os.Args
@@ -51,7 +76,9 @@ func main() {
 	line := scanner.Text()
 	input := strings.Split(line, ",")
 	for i := range len(input) {
-		count += get_valid_id(input[i])
+		count_part_1 += part1(input[i])
+		count_part_2 += part2(input[i])
 	}
-	fmt.Println("count:", count)
+	fmt.Println("count_part_1:", count_part_1)
+	fmt.Println("count_part_2:", count_part_2)
 }
