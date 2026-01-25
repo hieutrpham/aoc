1ce6a216151fa3b48b0b497e0a77dd5738b8973d
Hieu Pham <hieutrungpham91@gmail.com>
2026-01-25 09:44:15 +0200

    feat: day5 part2 done. i got cooked...

--- 2025/day5/day5.go
+++ 2025/day5/day5.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"cmp"
 	"fmt"
 	"os"
 	"slices"
@@ -44,15 +45,84 @@ func part1(id_range []string, ingredient_id []string) int {
 	return sum
 }
 
-func part2(id_range []string) int {
-	s := get_id_range(id_range)
-	var sorted_s []int
-	for i := range s {
-		sorted_s = append(sorted_s, s[i][0])
-		sorted_s = append(sorted_s, s[i][1])
+const max = 1
+const min = 0
+
+// checking if given a range of id []int overlap with the existing ranges [][]int
+// if detect overlap, return true
+func overlap(id_ranges [][]int, value_range []int) bool {
+	for i := range id_ranges {
+		// fmt.Println("LOG:", value_range, id_ranges[i])
+		if value_range[min] == id_ranges[i][min] && value_range[max] == id_ranges[i][max] {
+			return true
+		}
+		if !(value_range[min] > id_ranges[i][max] || value_range[max] < id_ranges[i][min]) {
+			return true
+		}
 	}
-	slices.Sort(sorted_s)
-	fmt.Println(sorted_s)
+	return false
+}
+
+// introducing the struct to hold ingredient id ranges and a boolean field to signal if it was counted when summing the ranges
+type ingredient struct {
+	id_range []int
+	included bool
+}
+
+func part2(id_range []string) int {
+	// array of ints converted from array of string
+	s := get_id_range(id_range)
+	slices.SortFunc(s, func(a, b []int) int {
+		return cmp.Compare(a[min], b[min])
+	})
+	// fmt.Println(s)
+	// array to store the overlap id ranges
+	var overlap_arr []ingredient
+	var rest_arr []ingredient
+
+	// for each range in s, loop through every item in s and check if there are overlap
+	for i := range s {
+		new := ingredient{s[i], false}
+		if overlap(s, s[i]) {
+			overlap_arr = append(overlap_arr, new)
+		} else {
+			rest_arr = append(rest_arr, new)
+		}
+	}
+	// fmt.Println(overlap_arr)
+	sum := 0
+	l_overlap := len(overlap_arr)
+	// for each range in overlap, check for every other ranges to see which ones overlapping each other, then summing the max - min + 1
+	for i := 0; i < l_overlap; i++ {
+		if !overlap_arr[i].included {
+
+			var s_arr [][]int
+			s_arr = append(s_arr, overlap_arr[i].id_range)
+
+			for j := 0; j < l_overlap; j++ {
+				if !overlap_arr[j].included && overlap(s_arr, overlap_arr[j].id_range) {
+					s_arr = append(s_arr, overlap_arr[j].id_range)
+					overlap_arr[j].included = true
+				}
+			}
+
+			var ret_arr []int
+			for i := range s_arr {
+				ret_arr = append(ret_arr, s_arr[i][min])
+				ret_arr = append(ret_arr, s_arr[i][max])
+			}
+
+			sum += slices.Max(ret_arr) - slices.Min(ret_arr) + 1
+			overlap_arr[i].included = true
+		}
+	}
+
+	// summing the non overlapping
+	for i := range rest_arr {
+		sum += rest_arr[i].id_range[max] - rest_arr[i].id_range[min] + 1
+	}
+
+	fmt.Println("sum:", sum)
 	return 0
 }
 
@@ -73,5 +143,5 @@ func main() {
 		}
 	}
 	// fmt.Println(part1(id_range, ingredient_id))
-	fmt.Println(part2(id_range))
+	part2(id_range)
 }
