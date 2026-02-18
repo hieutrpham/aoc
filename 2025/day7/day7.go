a1af00b985bf59128b643304ac981e74d9b89ff0
Hieu Pham <hieutrungpham91@gmail.com>
2026-02-18 08:13:16 +0200

    wip day7 part2

--- 2025/day7/day7.go
+++ 2025/day7/day7.go
@@ -13,7 +13,7 @@ func print_array[T any](input []T) {
 	}
 }
 
-func part1(input [][]string) int {
+func part1(input [][]string) ([][]string, int) {
 	row := len(input)
 	col := len(input[0])
 	count := 0
@@ -33,7 +33,7 @@ func part1(input [][]string) int {
 			}
 		}
 	}
-	return count
+	return input, count
 }
 
 func get_input() [][]string {
@@ -50,18 +50,69 @@ func get_input() [][]string {
 	return input
 }
 
+/*
+after each splitter, there are only 2 paths, left or right that a particle can go down
+
+		  |
+		  ^
+		 1 1 -> 1 way to get to each timeline. sum = 2
+		 ^ ^
+		1 2 1 -> 1 way to get to rear timeline. 2 ways in the middle one. sum = 4
+		^ ^ ^
+	   1 3 3 1
+	   ^ ^ _ ^
+	  1 4 331 1 -> if there is no previous splitter, don't add the 2 previous timelines
+*/
+/*
+how to build a binary tree?
+
+	a beam/pipe
+	a spliter?
+		a splitter denotes a new node if there is a beam on top
+	how to connect the nodes to create the tree?
+		one node could be connected to the node before by 1 column to the left or right and 2 rows up
+		each beam will have information of the node that created it
+		which means i have to create a struct of beam that store the node parent
+		the beam under each beam will have the same information as the one before it
+	looping through the input and construct the tree line by line
+		in order to know which node connects to which, i have to take a look back to the previous coord
+		that means when i create a node on row 2, i don't know how to connect the left and right node yet?
+		only when i move to row 4 that i can connect the node in row 2
+		add to the left node if based on the col parameter
+*/
+
+// coordinate of each node
 type Coord struct {
-	x int
-	y int
+	row int
+	col int
 }
 
+// node stores the pointer to the left and right node splitter
 type Node struct {
-	coord Coord
-	left  *Node
-	right *Node
+	coord    Coord
+	children []*Node
 }
 
+func newNode(row int, col int) Node {
+	return Node{coord: Coord{row, col}}
+}
 func main() {
 	input := get_input()
-	part1(input)
+	output, _ := part1(input)
+	nodes := make(map[Coord]*Node)
+	row_len := len(output)
+	col_len := len(output[0])
+	count := 0
+	for row := 0; row < row_len; row++ {
+		for col := 0; col < col_len; col++ {
+			if output[row][col] == "^" {
+				if output[row-1][col] == "|" {
+					new := newNode(row, col)
+					nodes[Coord{row, col}] = &new
+					count++
+				}
+			}
+		}
+	}
+	fmt.Println(nodes)
 }
